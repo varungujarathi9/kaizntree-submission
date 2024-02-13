432179310c6b83e62151f0fd595bf09da5591bc1
varungujarathi9 <v.gujarathi777@gmail.com>
2024-02-12 22:15:33 -0800

    fix: change DB to MySQL BREAKING CHANGE

--- kaizntree_backend/kaizntree_project/settings.py
+++ kaizntree_backend/kaizntree_project/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'kaizntree_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kaizndb',
+        'USER': 'admin',
+        'PASSWORD': 'admin1234',
+        'HOST': 'database-1.cjygqaksmzot.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
